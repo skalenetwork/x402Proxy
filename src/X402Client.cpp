123f4f83dced455af1be86bb6bbd964be8f4579f
Konstantin Kladko <13399135+kladkogex@users.noreply.github.com>
2025-09-23 20:04:26 +0100

    First commit

--- src/X402Client.cpp
+++ src/X402Client.cpp
@@ -1,7 +1,7 @@
 #include "X402Client.h"
 
-X402Client::X402Client(const std::string &connect_ip, uint16_t port)
-    : connectIp(connect_ip), port(port) {}
+X402Client::X402Client(const std::string &_connect_ip, uint16_t _port)
+    : connectIp(_connect_ip), port(_port) {}
 
 X402Client::~X402Client() {}
 
@@ -9,31 +9,31 @@ std::string X402Client::baseUrl() {
     return "http://" + connectIp;
 }
 
-std::string X402Client::parseStatusLineAndHeaders(const std::vector<std::string> &headersVector,
-                                                  std::map<std::string, std::string> &headersMap) {
+std::string X402Client::parseStatusLineAndHeaders(const std::vector<std::string> &_headersVector,
+                                                  std::map<std::string, std::string> &_headersMap) {
     std::string statusLine;
-    if (!headersVector.empty()) {
-        statusLine = headersVector[0];
+    if (!_headersVector.empty()) {
+        statusLine = _headersVector[0];
         statusLine.erase(statusLine.find_last_not_of(" \t\r\n") + 1);
     }
-    for (size_t i = 1; i < headersVector.size(); ++i) {
-        auto pos = headersVector[i].find(':');
-        if (pos != std::string::npos) {
-            std::string key = headersVector[i].substr(0, pos);
-            std::string value = headersVector[i].substr(pos + 1);
-            key.erase(0, key.find_first_not_of(" \t\r\n"));
-            key.erase(key.find_last_not_of(" \t\r\n") + 1);
-            value.erase(0, value.find_first_not_of(" \t\r\n"));
-            value.erase(value.find_last_not_of(" \t\r\n") + 1);
-            headersMap[key] = value;
+    for (size_t _i = 1; _i < _headersVector.size(); ++_i) {
+        auto _pos = _headersVector[_i].find(':');
+        if (_pos != std::string::npos) {
+            std::string _key = _headersVector[_i].substr(0, _pos);
+            std::string _value = _headersVector[_i].substr(_pos + 1);
+            _key.erase(0, _key.find_first_not_of(" \t\r\n"));
+            _key.erase(_key.find_last_not_of(" \t\r\n") + 1);
+            _value.erase(0, _value.find_first_not_of(" \t\r\n"));
+            _value.erase(_value.find_last_not_of(" \t\r\n") + 1);
+            _headersMap[_key] = _value;
         }
     }
     return statusLine;
 }
 
 std::tuple<std::map<std::string, std::string>, std::string, HttpResponse>
-X402Client::sendRequestAndParseResult(std::string location, const std::vector<std::string> &extraHeaders) {
-    auto resp = httpGet(baseUrl(), location, extraHeaders);
+X402Client::sendRequestAndParseResult(std::string _location, const std::vector<std::string> &_extraHeaders) {
+    auto resp = httpGet(baseUrl(), _location, _extraHeaders);
     auto headersVector = resp.headers;
     std::map<std::string, std::string> headersMap;
     auto statusLine = parseStatusLineAndHeaders(headersVector, headersMap);
@@ -45,27 +45,27 @@ X402Client::sendRequestAndParseResult(std::string location, const std::vector<st
     return {headersMap, statusLine, resp};
 }
 
-size_t X402Client::writeBody(char *ptr, size_t size, size_t nmemb, void *userdata) {
-    auto *out = static_cast<std::string *>(userdata);
-    out->append(ptr, size * nmemb);
-    return size * nmemb;
+size_t X402Client::writeBody(char *_ptr, size_t _size, size_t _nmemb, void *_userdata) {
+    auto *out = static_cast<std::string *>(_userdata);
+    out->append(_ptr, _size * _nmemb);
+    return _size * _nmemb;
 }
 
-size_t X402Client::writeHeader(char *buffer, size_t size, size_t nitems, void *userdata) {
-    auto *headers = static_cast<std::vector<std::string> *>(userdata);
-    headers->emplace_back(buffer, size * nitems);
-    return size * nitems;
+size_t X402Client::writeHeader(char *_buffer, size_t _size, size_t _nitems, void *_userdata) {
+    auto *headers = static_cast<std::vector<std::string> *>(_userdata);
+    headers->emplace_back(_buffer, _size * _nitems);
+    return _size * _nitems;
 }
 
 HttpResponse X402Client::httpGet(const std::string &_baseURL, const std::string &_location,
-                                 const std::vector<std::string> &extraHeaders) {
+                                 const std::vector<std::string> &_extraHeaders) {
     CURL *curl = curl_easy_init();
     if (!curl) throw std::runtime_error("curl_easy_init failed");
 
     std::string url = _baseURL + ":" + std::to_string(port) + "/" + _location;
 
     struct curl_slist *hdrs = nullptr;
-    for (auto &h: extraHeaders) hdrs = curl_slist_append(hdrs, h.c_str());
+    for (auto &_h: _extraHeaders) hdrs = curl_slist_append(hdrs, _h.c_str());
 
     HttpResponse resp;
     curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
--- src/X402Client.h
+++ src/X402Client.h
@@ -1,22 +1,16 @@
 #pragma once
 
 #include "../src/common.h"
-
-
 #include <folly/init/Init.h>
 #include <folly/SocketAddress.h>
 #include <folly/init/Init.h>
 #include <proxygen/httpserver/HTTPServer.h>
-
 #include "ServerFactory.h"
-
-// ---- libcurl helper ---------------------------------------------------------
 #include <curl/curl.h>
 #include <nlohmann/json.hpp>
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/beast/version.hpp>
-
 
 struct HttpResponse {
     long status = 0;
@@ -30,20 +24,20 @@ private:
     uint16_t port;
 
 public:
-    X402Client(const std::string &connect_ip, uint16_t port);
+    X402Client(const std::string &_connectIp, uint16_t _port);
     ~X402Client();
 
     std::string baseUrl();
-    static std::string parseStatusLineAndHeaders(const std::vector<std::string> &headersVector,
-                                                 std::map<std::string, std::string> &headersMap);
+    static std::string parseStatusLineAndHeaders(const std::vector<std::string> &_headersVector,
+                                                 std::map<std::string, std::string> &_headersMap);
     std::tuple<std::map<std::string, std::string>, std::string, HttpResponse>
-    sendRequestAndParseResult(std::string location, const std::vector<std::string> &extraHeaders);
+    sendRequestAndParseResult(std::string _location, const std::vector<std::string> &_extraHeaders);
 
-    static size_t writeBody(char *ptr, size_t size, size_t nmemb, void *userdata);
-    static size_t writeHeader(char *buffer, size_t size, size_t nitems, void *userdata);
+    static size_t writeBody(char *_ptr, size_t _size, size_t _nmemb, void *_userdata);
+    static size_t writeHeader(char *_buffer, size_t _size, size_t _nitems, void *_userdata);
 
     HttpResponse httpGet(const std::string &_baseURL, const std::string &_location,
-                         const std::vector<std::string> &extraHeaders);
+                         const std::vector<std::string> &_extraHeaders);
 };
 
 
